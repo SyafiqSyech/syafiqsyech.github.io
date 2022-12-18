fd36543a8e6dea5fd1cbdcb2de1c1260368c29a5
SyafiqSyech <106647971+SyafiqSyech@users.noreply.github.com>
2022-12-18 15:43:00 +0700

    cleanup

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const hamburg = document.querySelector(".hamburg");
-let lastScrollY = window.scrollY;
 
-window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-        hamburg.classList.add("hamburg--hidden");
-    } else {
-        hamburg.classList.remove("hamburg--hidden");
-    }
-
-    lastScrollY = window.scrollY;
-});
+function ImgClick(n){
+    document.getElementById("im"+n).classList.toggle("imgtoggle")
+    document.getElementById("tx"+n).classList.toggle("desctoggle")
+}
