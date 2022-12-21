4ee4b463b58ca5bf0f0666f242bf08fb02d201ef
SyafiqSyech <106647971+SyafiqSyech@users.noreply.github.com>
2022-12-21 11:47:34 +0700

    updates

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ function ImgClick(n){
     document.getElementById("im"+n).classList.toggle("imgtoggle")
     document.getElementById("tx"+n).classList.toggle("desctoggle")
 }
+
+function MenuClick(){
+    document.getElementById("menupopup").classList.toggle("menu_pop_open")
+}
