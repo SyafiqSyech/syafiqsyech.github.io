965adcf6cb85aa8c14c36c92ab7bb3215f8f366f
SyafiqSyech <106647971+SyafiqSyech@users.noreply.github.com>
2022-12-23 18:03:32 +0700

    home page update

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.addEventListener("scroll", () => {
+    if(window.scrollY <= 50) {
+        document.getElementById("arrowdown").classList.remove("faded")
+    }
+    else{
+        document.getElementById("arrowdown").classList.add("faded")
+    }
+});
 
 function ImgClick(n){
     document.getElementById("im"+n).classList.toggle("imgtoggle")
@@ -6,6 +14,7 @@ function ImgClick(n){
 
 function MenuClick(){
     document.getElementById("menupopup").classList.toggle("menu_pop_open")
+    document.getElementById("menupopupside").classList.toggle("menu_pop_side_open")
     document.getElementById("menupopupbox").classList.toggle("menu_pop_box_open")
     document.body.classList.toggle("stop_scroll");
 }
